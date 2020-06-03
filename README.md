641ceb37dc0ea9f919f938d6c8e7746b887bc1c2
unknown <vaibhav.kaul2@gnmail.com>
2020-06-03 15:28:37 +0530

    Search/Delete/Edit added

--- README.md
+++ README.md
@@ -7,17 +7,27 @@ Calen-Clone is a replica of Google Calender.The project is hosted on Github http
 * [Javascript](https://www.w3schools.com/js/js_intro.asp) - Used for creating and implementing the functionality like adding an event.
 * [Html](https://www.w3schools.com/html/) - Used for creating the entire skeletal interface
 * [Bootstrap CDN](https://getbootstrap.com/) - Used for implementing the calender and other responsiveness.
-* [LocalStorage API](https://developer.mozilla.org/en-US/docs/Web/API/Window/localStorage)- Used for sending SMS.
+* [LocalStorage API](https://developer.mozilla.org/en-US/docs/Web/API/Window/localStorage)- Used for storing the event locally in the browser.
 
 # How it works?
+
+### On the index.html page ###
+
 1. *First the user needs to select any month and year from the calender*
 2. *I have provided 2 arrows above the calender to go to the previous and next years.*
 3. *Once the user has selected a particular month then he can click on the specific date of adding event.*
 4. *Once you click on the date an Add Event Dialog Box appears.*
-4. *You can fill the Name, Description, Time of the event as well as you can switch On notifications for an event by checking the Add Notification button.*
+4. *You can fill the Name, Description, Time of the event as well as you can switch On notifications for an event by checking the Add           Notification button.*
 5. *Click on Add Event button and your visits gets added.*
-6. *To Edit/Delete/View an event click on ☰ button and open a sidebar and visit the Show Events button*
-7. *Once you are on the View Events page click on the logo image on left side of Navbar to go back to homepage.*
+6. *To View an event click on right most Show Saved Events button on navbar*
+
+### On events.html page ###
+
+1. *Once you are on the View Events page click on the logo image on left side of Navbar to go back to homepage.*
+2. *To search an event search it in the search box on the top right of navbar in events.html file*
+3. *To delete an event click on the X icon on the bottom left of the Event-Card*
+4. *To Edit an event click on the PENCIL ICON on the bottom left of Event-Card*
+5. *To save the edit click in the RED TICK on the bottom left of Event-Card*
 
 # Installation #
 ---
@@ -27,11 +37,36 @@ Calen-Clone is a replica of Google Calender.The project is hosted on Github http
 2. Go to the above website and scroll down to find 4 links.
 ![Bootstrap CDN](https://i.imgur.com/3Sevt3E.png)
 
-3. Copy the < CSS only > CDN link into the *head* tag of your webpage.
+3. Copy the < CSS only > CDN link into the *head* tag of your webpage in both (events.html and indx.html).
 
-4. Copy the < JS, Popper.js, and jQuery > CDN link just after the ending *body* tag of your webpage.
+4. Copy the < JS, Popper.js, and jQuery > CDN link just after the ending *body* tag of your webpage in both (events.html and indx.html).
    
 5. The file mention in point 4 should be added in the sequence  jQuery first, then Popper.js, then Bootstrap JS.
+
+### If you are unable to find the files then i have provided a direct link to the below ###
+
+*Bootstrap CDN CSS* 
+
+ <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css" 
+    integrity="sha384-9aIt2nRpC12Uk9gS9baDl411NQApFmC26EwAOH8WgZl5MYYxFfc+NcPb1dKGj7Sk" crossorigin="anonymous">
+
+*Bootstrap CDN Jquery File*
+
+<script src="https://code.jquery.com/jquery-3.3.1.slim.min.js"
+        integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo"
+        crossorigin="anonymous"></script>
+
+*Bootstrap CDN Popper.js File*
+
+<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.0/umd/popper.min.js"
+        integrity="sha384-cs/chFZiN24E4KMATLdqdvsezGxaGsi4hLGOzlXwp5UZB1LY//20VyM2taTB4QvJ"
+        crossorigin="anonymous"></script>
+
+*Bootstrap CDN bootstrap.min.js File*    
+
+<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.0/js/bootstrap.min.js"
+        integrity="sha384-uefMccjFJAIv6A+rW+L4AHf99KvxDjWSu1z9VI8SKNVmz4sk7buKt/6v9KI65qnm"
+        crossorigin="anonymous"></script>
 
 # Structure
 ---
@@ -49,6 +84,11 @@ Calen-Clone is a replica of Google Calender.The project is hosted on Github http
 ### Explore Events Page ###
 
 ![ExploreEvents](https://i.imgur.com/WRvhrl6.png)
+
+ ### Access the Events Data From LocalStorage in Browser ### 
+ *You will only see some content there when you add any event other wise it will stay empty*
+
+ ![Acces the Events Data](https://i.imgur.com/AIvamXh.mp4)
 
 # Deployment
 This app is deployed at Github-Pages.  https://vaibhavkaul69.github.io/frontend-vaibhav-kaul/
--- js/index.js
+++ js/index.js
@@ -3,11 +3,15 @@ let currentMonth = today.getMonth();
 let currentYear = today.getFullYear();
 let selectYear = document.getElementById("year");
 let selectMonth = document.getElementById("month");
-let tbl = document.getElementById("calendar-body"); 
+let calenderBody = document.getElementById("calendar-body"); 
 let months = ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"];
 let monthAndYear = document.getElementById("monthAndYear");
 
-
+let eventCount=localStorage.getItem('evt-count');
+if(eventCount === null || eventCount === undefined){
+    eventCount=0;
+    localStorage.setItem('evt-count',eventCount);
+};
 
 const next=()=> {
     currentYear = (currentMonth === 11) ? currentYear + 1 : currentYear;
@@ -21,7 +25,7 @@ const previous=()=> {
     showCalendar(currentMonth, currentYear);
 };
 
-const jump=()=> {
+const jumpToYear=()=> {
     currentYear = parseInt(selectYear.value);
     currentMonth = parseInt(selectMonth.value);
     showCalendar(currentMonth, currentYear);
@@ -32,7 +36,7 @@ const showCalendar=(month, year)=> {
     let firstDay = (new Date(year, month)).getDay();
     let daysInMonth = 32 - new Date(year, month, 32).getDate();
     // clearing all previous cells
-    tbl.innerHTML = "";
+    calenderBody.innerHTML = "";
     // filing data about month and in the page via DOM.
     monthAndYear.textContent = months[month] + " " + year;
     selectYear.value = year;
@@ -42,42 +46,42 @@ const showCalendar=(month, year)=> {
     let date = 1;
     for (let i = 0; i < 6; i++) {
         // creates a table row
-        let row = document.createElement("tr");
+        let rowCalender = document.createElement("tr");
 
         //creating individual cells, filing them up with data.
         for (let j = 0; j < 7; j++) {
             if (i === 0 && j < firstDay) {
-                let cell = document.createElement("td");
-                let cellText = document.createTextNode("");
-                cell.appendChild(cellText);
-                row.appendChild(cell);
+                let cellCalender = document.createElement("td");
+                let cellTextCalender = document.createTextNode("");
+                cellCalender.appendChild(cellTextCalender);
+                rowCalender.appendChild(cellCalender);
             }
             else if (date > daysInMonth) {
                 break;
             }
 
             else {
-                let cell = document.createElement("td");
-                let cellText = document.createTextNode(date);
+                let cellCalender = document.createElement("td");
+                let cellTextCalender = document.createTextNode(date);
                 if (date === today.getDate() && year === today.getFullYear() && month === today.getMonth()) {
-                    cell.classList.add("bg-info");
+                    cellCalender.classList.add("bg-info");
                 } // color today's date
-                cell.appendChild(cellText);
-                row.appendChild(cell);
+                cellCalender.appendChild(cellTextCalender);
+                rowCalender.appendChild(cellCalender);
                 date++;
             }
 
 
         }
 
-        tbl.appendChild(row); // appending each row into calendar body.
+        calenderBody.appendChild(rowCalender); // appending each row into calendar body.
     }
     return {month:months[month],year:year};
 
 };
 showCalendar(currentMonth, currentYear);
 //Create an Event Modal Dialog box.
-//This code snippet is available on https://getbootstrap.com/docs/4.0/components/modal/ .
+
 const createAddEventModal=(monthName)=>{
     return `
     <div class="modal fade" id="${monthName.month}_${event.target.textContent}" tabindex="-1" role="dialog" aria-labelledby="exampleModalCenterTitle" aria-hidden="true">
@@ -92,7 +96,7 @@ const createAddEventModal=(monthName)=>{
         <div class="modal-body">
         <form>
         <div class='form-group'>
-            <input class='form-control' type='text' disabled placeholder='${monthName.month}-${event.target.textContent}, ${monthName.year} '/>
+            <input class='form-control' id='disabled-date' type='text' disabled placeholder='${monthName.month}-${event.target.textContent}'/>
         </div>
         <div class="form-group">
             <label for="inputEventName">Name</label>
@@ -105,54 +109,54 @@ const createAddEventModal=(monthName)=>{
         <div class="form-group">
             <label for="inputEventTime">Select Event Time</label>
             <select  class="form-control" id="inputEventTime">
-                <option value="00:00">12.00 AM</option>
-                <option value="00:30">12.30 AM</option>
-                <option value="01:00">01.00 AM</option>
-                <option value="01:30">01.30 AM</option>
-                <option value="02:00">02.00 AM</option>
-                <option value="02:30">02.30 AM</option>
-                <option value="03:00">03.00 AM</option>
-                <option value="03:30">03.30 AM</option>
-                <option value="04:00">04.00 AM</option>
-                <option value="04:30">04.30 AM</option>
-                <option value="05:00">05.00 AM</option>
-                <option value="05:30">05.30 AM</option>
-                <option value="06:00">06.00 AM</option>
-                <option value="06:30">06.30 AM</option>
-                <option value="07:00">07.00 AM</option>
-                <option value="07:30">07.30 AM</option>
-                <option value="08:00">08.00 AM</option>
-                <option value="08:30">08.30 AM</option>
-                <option value="09:00">09.00 AM</option>
-                <option value="09:30">09.30 AM</option>
-                <option value="10:00">10.00 AM</option>
-                <option value="10:30">10.30 AM</option>
-                <option value="11:00">11.00 AM</option>
-                <option value="11:30">11.30 AM</option>
-                <option value="12:00">12.00 PM</option>
-                <option value="12:30">12.30 PM</option>
-                <option value="13:00">01.00 PM</option>
-                <option value="13:30">01.30 PM</option>
-                <option value="14:00">02.00 PM</option>
-                <option value="14:30">02.30 PM</option>
-                <option value="15:00">03.00 PM</option>
-                <option value="15:30">03.30 PM</option>
-                <option value="16:00">04.00 PM</option>
-                <option value="16:30">04.30 PM</option>
-                <option value="17:00">05.00 PM</option>
-                <option value="17:30">05.30 PM</option>
-                <option value="18:00">06.00 PM</option>
-                <option value="18:30">06.30 PM</option>
-                <option value="19:00">07.00 PM</option>
-                <option value="19:30">07.30 PM</option>
-                <option value="20:00">08.00 PM</option>
-                <option value="20:30">08.30 PM</option>
-                <option value="21:00">09.00 PM</option>
-                <option value="21:30">09.30 PM</option>
-                <option value="22:00">10.00 PM</option>
-                <option value="22:30">10.30 PM</option>
-                <option value="23:00">11.00 PM</option>
-                <option value="23:30">11.30 PM</option>
+                <option value="00:00 AM">12.00 AM</option>
+                <option value="00:30 AM">12.30 AM</option>
+                <option value="01:00 AM">01.00 AM</option>
+                <option value="01:30 AM">01.30 AM</option>
+                <option value="02:00 AM">02.00 AM</option>
+                <option value="02:30 AM">02.30 AM</option>
+                <option value="03:00 AM">03.00 AM</option>
+                <option value="03:30 AM">03.30 AM</option>
+                <option value="04:00 AM">04.00 AM</option>
+                <option value="04:30 AM">04.30 AM</option>
+                <option value="05:00 AM">05.00 AM</option>
+                <option value="05:30 AM">05.30 AM</option>
+                <option value="06:00 AM">06.00 AM</option>
+                <option value="06:30 AM">06.30 AM</option>
+                <option value="07:00 AM">07.00 AM</option>
+                <option value="07:30 AM">07.30 AM</option>
+                <option value="08:00 AM">08.00 AM</option>
+                <option value="08:30 AM">08.30 AM</option>
+                <option value="09:00 AM">09.00 AM</option>
+                <option value="09:30 AM">09.30 AM</option>
+                <option value="10:00 AM">10.00 AM</option>
+                <option value="10:30 AM">10.30 AM</option>
+                <option value="11:00 AM">11.00 AM</option>
+                <option value="11:30 AM">11.30 AM</option>
+                <option value="12:00 PM">12.00 PM</option>
+                <option value="12:30 PM">12.30 PM</option>
+                <option value="13:00 PM">01.00 PM</option>
+                <option value="13:30 PM">01.30 PM</option>
+                <option value="14:00 PM">02.00 PM</option>
+                <option value="14:30 PM">02.30 PM</option>
+                <option value="15:00 PM">03.00 PM</option>
+                <option value="15:30 PM">03.30 PM</option>
+                <option value="16:00 PM">04.00 PM</option>
+                <option value="16:30 PM">04.30 PM</option>
+                <option value="17:00 PM">05.00 PM</option>
+                <option value="17:30 PM">05.30 PM</option>
+                <option value="18:00 PM">06.00 PM</option>
+                <option value="18:30 PM">06.30 PM</option>
+                <option value="19:00 PM">07.00 PM</option>
+                <option value="19:30 PM">07.30 PM</option>
+                <option value="20:00 PM">08.00 PM</option>
+                <option value="20:30 PM">08.30 PM</option>
+                <option value="21:00 PM">09.00 PM</option>
+                <option value="21:30 PM">09.30 PM</option>
+                <option value="22:00 PM">10.00 PM</option>
+                <option value="22:30 PM">10.30 PM</option>
+                <option value="23:00 PM">11.00 PM</option>
+                <option value="23:30 PM">11.30 PM</option>
             </select>
           </div>
           <div class="form-group">
@@ -162,7 +166,7 @@ const createAddEventModal=(monthName)=>{
     </form>   
         </div>
         <div class="modal-footer">
-          <button type="button" id='add-event' onclick='addEventBtn(this.${monthName.month}_${event.target.textContent})' class="btn  btn-outline-success">Add Event</button>
+          <button type="button" id='add-event' data-dismiss="modal" onclick='addEventBtn(this.${monthName.month}_${event.target.textContent})' class="btn  btn-outline-success">Add Event</button>
         </div>
       </div>
     </div>
@@ -170,7 +174,7 @@ const createAddEventModal=(monthName)=>{
     `;
 }
 
-tbl.addEventListener('click',(event)=>{
+calenderBody.addEventListener('click',(event)=>{
 
     //store the current month and year in the right calender in form of object.
     const monthName=showCalendar(currentMonth, currentYear);
@@ -202,35 +206,25 @@ tbl.addEventListener('click',(event)=>{
 const addEventBtn=()=>{
     console.log('Btn clicked');
     //Get the input from Create Event Modal Box.
-    const eventName=document.getElementById('inputEventName').value;
+    let eventName=document.getElementById('inputEventName').value;
+    eventName=eventName.toUpperCase();
     const eventDescription=document.getElementById('inputEventDescription').value;
     const eventTime=document.getElementById('inputEventTime').value;
-    const eventDate=showCalendar(currentMonth,currentYear);
+    const eventDate=document.getElementById('disabled-date').placeholder;
     //Create an object of all the input items
     let eventObject={
         name:eventName,
         description:eventDescription,
         time:eventTime,
-        date:eventDate.month
+        date:eventDate
     }
-    localStorage.setItem('event-count',++eventCount);
-    localStorage.setItem('event-'+localStorage.getItem('event-count'),JSON.stringify(eventObject));
+    localStorage.setItem('evt-count',++eventCount);
+    localStorage.setItem('event-'+localStorage.getItem('evt-count'),JSON.stringify(eventObject));
 
     //Store the inputs to the local storage so as to retrieve them back when we want.
     console.log(`${eventName} ${eventDescription} ${eventTime}`);
+    
 };
 
-document.querySelector('.toggle-sidebar').addEventListener('click',()=>{
-    if(document.querySelector('.left_sidebar_calender').classList.contains('active')){
-        document.querySelector('.left_sidebar_calender').style.opacity='0';
-        document.querySelector('.left_sidebar_calender').classList.remove('active');
-
-    }
-    else{
-        document.querySelector('.left_sidebar_calender').style.opacity='1';
-        document.querySelector('.left_sidebar_calender').classList.add('active');
-    }
-    
-});
 
 
