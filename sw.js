843afc46393ff02e3c491ac80903d366571b6259
unknown <vaibhav.kaul2@gnmail.com>
2020-06-04 23:56:50 +0530

    PWA Feature Added

--- sw.js
+++ sw.js
@@ -2,10 +2,10 @@ const staticCacheArray='calen-v4';
 const dynamicCacheArray='calen-dyn-v1';
 const assets=[
     '/',
-    '/index.html',
-    '/events.html',
-    '/manifest.json',
-    '/js/events.js',
+    'index.html',
+    'events.html',
+    'manifest.json',
+    'js/events.js',
     'https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css ',
     'https://code.jquery.com/jquery-3.3.1.slim.min.js',
     'https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.0/umd/popper.min.js',
